16814c3cfe9294e1b1c545f0ec97126bf14e3e4b
jeremyFERNANDES <fernandes.jerem@gmail.com>
2016-12-06 18:27:55 +0100

    Amelioration header

--- ProjetEcom/WebContent/js/main.js
+++ ProjetEcom/WebContent/js/main.js
@@ -671,6 +671,44 @@ app.service('elasticSearchSuggestion', function (esFactory) {
 	  });
 });
 
+app.directive('hoverPopover2', function ($compile, $templateCache, $timeout, $rootScope, $compile) {
+	var getTemplate = function (contentType) {
+	    return $templateCache.get('templatePopoverMonCompte.html');
+	};
+	return {
+	    restrict: 'A',
+	    link: function (scope, element, attrs) {
+	    	
+	        var content = getTemplate();
+	        var compileContent = $compile(content)(scope);
+	        $rootScope.insidePopover = false;
+	        $(element).popover({
+	            content: compileContent,
+	            placement: 'bottom',
+	            html: true
+	        });
+            
+	        $("#monCompteHeader").hover(function(){
+	        	$(element).popover('show');
+                scope.attachEvents(element);
+	        });
+	        
+	    },
+	    controller: function ($scope, $element) {
+	        $scope.attachEvents = function (element) {
+	            $('.popover').on('mouseenter', function () {
+	                $rootScope.insidePopover = true;
+	            });
+	            $('.popover').on('mouseleave', function () {
+	                $rootScope.insidePopover = false;
+	                $(element).popover('hide');
+	            });
+	        }
+	    }
+	    
+	};
+});
+
 
 app.directive('hoverPopover', function ($compile, $templateCache, $timeout, $rootScope, $compile) {
 	var getTemplate = function (contentType) {
