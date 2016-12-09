3f3b13a97f8cde749723d3f732d2ba93d7d212f0
JulienArmand <julien.armand26@gmail.com>
2016-12-09 09:26:21 +0100

    Update modification profil pour éviter champs vides

--- ProjetEcom/WebContent/js/compteClientCtrl.js
+++ ProjetEcom/WebContent/js/compteClientCtrl.js
@@ -17,36 +17,45 @@ routeAppControllers.controller("compteClient", function($scope, $http, $location
 			"prenom" : $("#prenom").val(),
 			"email" : $("#email").val()
 			}}).then(function(response) {
-				if(response.data === ('doubleExiste')){
-					document.getElementById('erreurDoubleExiste').style.display = "block";
-					document.getElementById('erreurEmailExiste').style.display = "none";
-					document.getElementById('erreurPseudoExiste').style.display = "none";
-					document.getElementById('confirmationModificationProfil').style.display = "none";
-				}
-				else{
-					if(response.data === ('pseudoExiste')){
+				switch(response.data){
+					case('champVide'):
+						document.getElementById('erreurChampVide').style.display = "block";
+						document.getElementById('erreurEmailExiste').style.display = "none";
+						document.getElementById('erreurPseudoExiste').style.display = "none";
+						document.getElementById('erreurDoubleExiste').style.display = "none";
+						document.getElementById('confirmationModificationProfil').style.display = "none";
+						break;
+					case ('doubleExiste'):
+						document.getElementById('erreurDoubleExiste').style.display = "block";
+						document.getElementById('erreurEmailExiste').style.display = "none";
+						document.getElementById('erreurPseudoExiste').style.display = "none";
+						document.getElementById('confirmationModificationProfil').style.display = "none";
+						document.getElementById('erreurChampVide').style.display = "none";
+						break;
+					case('pseudoExiste'):
 						document.getElementById('erreurPseudoExiste').style.display = "block";
 						document.getElementById('erreurEmailExiste').style.display = "none";
 						document.getElementById('erreurDoubleExiste').style.display = "none";
 						document.getElementById('confirmationModificationProfil').style.display = "none";
-					}
-					else{
-						if(response.data ===('emailExiste')){
-							document.getElementById('monCompteHeader').innerHTML='Bienvenue '.concat($("#pseudo").val()).concat('<i class="glyphicon glyphicon-chevron-down">');
-							document.getElementById('erreurEmailExiste').style.display = "block";
-							document.getElementById('erreurPseudoExiste').style.display = "none";
-							document.getElementById('erreurDoubleExiste').style.display = "none";
-							document.getElementById('confirmationModificationProfil').style.display = "none";
-						}
-						else{
-							document.getElementById('monCompteHeader').innerHTML='Bienvenue '.concat($("#pseudo").val()).concat('<i class="glyphicon glyphicon-chevron-down">');
-							document.getElementById('confirmationModificationProfil').style.display = "block";
-							document.getElementById('erreurPseudoExiste').style.display = "none";
-							document.getElementById('erreurEmailExiste').style.display = "none";
-							document.getElementById('erreurDoubleExiste').style.display = "none";
-						}
-					}	
-				}	
+						document.getElementById('erreurChampVide').style.display = "none";
+						break;
+					case('emailExiste'):
+						document.getElementById('monCompteHeader').innerHTML='Bienvenue '.concat($("#pseudo").val()).concat('<i class="glyphicon glyphicon-chevron-down">');
+						document.getElementById('erreurEmailExiste').style.display = "block";
+						document.getElementById('erreurPseudoExiste').style.display = "none";
+						document.getElementById('erreurDoubleExiste').style.display = "none";
+						document.getElementById('confirmationModificationProfil').style.display = "none";
+						document.getElementById('erreurChampVide').style.display = "none";
+						break;
+					default:
+						document.getElementById('monCompteHeader').innerHTML='Bienvenue '.concat($("#pseudo").val()).concat('<i class="glyphicon glyphicon-chevron-down">');
+						document.getElementById('confirmationModificationProfil').style.display = "block";
+						document.getElementById('erreurPseudoExiste').style.display = "none";
+						document.getElementById('erreurEmailExiste').style.display = "none";
+						document.getElementById('erreurDoubleExiste').style.display = "none";
+						document.getElementById('erreurChampVide').style.display = "none";
+						break;
+				}
 	    });
 	}
 	
