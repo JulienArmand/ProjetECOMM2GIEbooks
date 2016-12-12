41eb650ca2e1b52abde334df3ef4f96ec750824e
JulienArmand <julien.armand26@gmail.com>
2016-12-12 17:00:37 +0100

    Vérification des éléments d'inscription

--- ProjetEcom/WebContent/js/inscriptionCtrl.js
+++ ProjetEcom/WebContent/js/inscriptionCtrl.js
@@ -1,19 +1,51 @@
 
+function checkCaracteresInterdits(value){
+	result = false;
+	if(/^[a-zA-Z0-9- ]*$/.test(value) == false) {
+		result = true;
+	}
+	return result;
+}
+
+function validationEmail(mail){ 
+	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)){
+		return (false);
+	}  
+    return (true);
+}  
+
 routeAppControllers.controller("inscriptionCtrl", function($scope, $http,$routeParams,$rootScope){	
-	
-	$("#menu").hide();
-	
+	$("#menu").hide();	
 	$scope.rez= true;
 	$scope.emailSave = "hello";
 	$scope.submit = function() {
-		$http.get("InscriptionClient",{params:{"identifiant":this._id,"nom":this._nom,"prenom":this._prenom,"motdepasse":this._mdp,"motdepasseconfirm":this._mdpc,"email":this._email}}).then(function(response) {
-			var data = response.data;
-			$scope.rez = data;
-			
-		});
-		$scope._email=this._email;
-		this._mdp='';
-		this._mdpc='';
-      };
-	
+		document.getElementById('erreurCaracteresInterdits').style.display = "none";
+		document.getElementById('erreurMotDePasseTropCourt').style.display = "none";
+		document.getElementById('erreurMailNonValide').style.display = "none";
+		//Teste si l'un des champs contient des caractères interdits
+		if(checkCaracteresInterdits(this._id) || checkCaracteresInterdits(this._nom) || checkCaracteresInterdits(this._prenom) || checkCaracteresInterdits(this._mdp) || checkCaracteresInterdits(this._mdpc)){
+			document.getElementById('erreurCaracteresInterdits').style.display = "block";
+		}
+		else{
+			//Teste si le mot de passe est trop court
+			if(this._mdp.length < 9 || this._mdpc.length < 9){
+				document.getElementById('erreurMotDePasseTropCourt').style.display = "block";
+			}
+			else{
+				//Teste si l'adresse mail est valide
+				if(validationEmail(this._email)){
+					document.getElementById('erreurMailNonValide').style.display = "block";
+				}
+				else{
+					$http.get("InscriptionClient",{params:{"identifiant":this._id,"nom":this._nom,"prenom":this._prenom,"motdepasse":this._mdp,"motdepasseconfirm":this._mdpc,"email":this._email}}).then(function(response) {
+						var data = response.data;
+						$scope.rez = data;
+						});
+					$scope._email=this._email;
+					this._mdp='';
+					this._mdpc='';
+				}
+			}		
+		}
+     };
 });
