e5c420abd9410bf85254f0d9ba063b39e0a4bcce
soulierc <clement.soulier@e.ujf-grenoble.fr>
2016-12-13 09:29:04 +0100

    Ajout d'une servlet pour les test de charge

---
+++ ProjetEcom/src/servlets/TestChargeServlet.java
@@ -0,0 +1,47 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import beans.GestionAuteur;
+import beans.GestionAvis;
+import beans.GestionClient;
+import beans.GestionCommande;
+import beans.GestionEditeur;
+import beans.GestionGenre;
+import beans.GestionLivre;
+import beans.GestionPromotion;
+import beans.GestionVente;
+import model.Auteur;
+import model.Avis;
+import model.Client;
+import model.Commande;
+import model.Editeur;
+import model.Genre;
+import model.Livre;
+import model.Promotion;
+import model.Vente;
+
+public class TestChargeServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -3401197851263972685L;
+	
+	@EJB()  
+	private GestionLivre myBean; 
+		
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		List<Livre> l = myBean.getLesLivresEnPromotion();
+		
+	}
+
+}
