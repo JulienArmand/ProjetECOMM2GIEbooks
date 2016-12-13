40e48c1b6c3b345364385882fa9f4f5f395bd929
ochiers <sebastien.ochier@gmail.com>
2016-12-13 13:55:29 +0100

    Modification testMail

--- ProjetEcom/src/servlets/EnvoiMailServlet.java
+++ ProjetEcom/src/servlets/EnvoiMailServlet.java
@@ -52,7 +52,7 @@ public class EnvoiMailServlet extends HttpServlet {
 		props.setProperty("mail.smtp.host", "localhost");
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.host", "smtp.ujf-grenoble.fr");
 		props.put("mail.smtp.port", "587");
 
 		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
