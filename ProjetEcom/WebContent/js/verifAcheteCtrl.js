ebfc47d042a9651d0b958ed862936cf098e33235
ochiers <sebastien.ochier@gmail.com>
2016-12-14 13:32:13 +0100

    MAJ test

--- ProjetEcom/WebContent/js/verifAcheteCtrl.js
+++ ProjetEcom/WebContent/js/verifAcheteCtrl.js
@@ -1,6 +1,7 @@
 app.controller("verifAcheteCtrl", function($scope, $http, $rootScope, ngCart){
 		
 	$scope.estAchete = function(id){
+		console.log("estAchete");
 		var b = false;
 		if($rootScope.estConnecte){
 			angular.forEach($rootScope.commandes, function(value){
